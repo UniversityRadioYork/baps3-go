339c918943e9d0baf180d195e55e8e8fbd7bd9f3
Sam Willcocks <sam@wlcx.cc>
2015-03-28 18:31:38 +0000

    Add servicestate from bifrost

---
+++ service_state.go
@@ -0,0 +1,135 @@
+package baps3
+
+// The part of the BAPS3 connector code responsible for updating the
+// internal state.
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// serviceState is the struct of all known state for a BAPS3 service.
+// TODO(CaptainHayashi): possibly segregate more by feature, so elements not
+// relevant to the current feature set aren't allocated?
+type serviceState struct {
+	// Core
+	features map[Feature]struct{}
+	state    string
+
+	// TimeReport
+	time time.Duration
+
+	// FileLoad
+	file string
+}
+
+// initServiceState creates a new, blank, serviceState.
+func initServiceState() (s *serviceState) {
+	s = new(serviceState)
+	s.features = make(map[Feature]struct{})
+	s.state = "Ready"
+
+	return
+}
+
+// update updates a serviceState according to an incoming service response.
+func (s *serviceState) update(res Message) (err error) {
+	switch res.Word() {
+	case RsFeatures:
+		err = s.updateFeaturesFromMessage(res)
+	case RsFile:
+		err = s.updateFileFromMessage(res)
+	case RsState:
+		err = s.updateStateFromMessage(res)
+	case RsTime:
+		err = s.updateTimeFromMessage(res)
+	}
+
+	return
+}
+
+func (s *serviceState) updateFeaturesFromMessage(res Message) (err error) {
+	feats := make(map[Feature]struct{})
+
+	for i := 0; ; i++ {
+		if fstring, e := res.Arg(i); e == nil {
+			feat := LookupFeature(fstring)
+			if feat == FtUnknown {
+				err = fmt.Errorf("unknown feature: %q", fstring)
+				break
+			}
+			feats[feat] = struct{}{}
+		} else {
+			// e != nil means we've run out of arguments.
+			break
+		}
+	}
+
+	s.features = feats
+	return
+}
+
+func (s *serviceState) updateFileFromMessage(res Message) (err error) {
+	// Expecting only one argument
+	if _, err := res.Arg(1); err == nil {
+		return fmt.Errorf("too many arguments in %q", res)
+	}
+
+	file, err := res.Arg(0)
+	if err != nil {
+		s.file = ""
+		return
+	}
+
+	s.file = file
+
+	return
+}
+
+func (s *serviceState) updateStateFromMessage(res Message) (err error) {
+	// Expecting only one argument
+	if _, err := res.Arg(1); err == nil {
+		return fmt.Errorf("too many arguments in %q", res)
+	}
+
+	state, err := res.Arg(0)
+	if err != nil {
+		// TODO(CaptainHayashi): "Ready" is currently the most
+		// valid fallback value in the spec.  Does the spec
+		// need an 'I don't know what the state is' value?
+		s.state = "Ready"
+		return
+	}
+
+	s.state = state
+
+	return
+}
+
+func (s *serviceState) updateTimeFromMessage(res Message) (err error) {
+	// Expecting only one argument
+	if _, err := res.Arg(1); err == nil {
+		return fmt.Errorf("too many arguments in %q", res)
+	}
+
+	usecs, err := res.Arg(0)
+	if err != nil {
+		return
+	}
+
+	usec, err := strconv.Atoi(usecs)
+	if err != nil {
+		return
+	}
+
+	s.time = time.Duration(usec) * time.Microsecond
+
+	return
+}
+
+// hasFeature returns whether the connected server advertises the given feature.
+func (s *serviceState) hasFeature(f Feature) bool {
+	_, ok := s.features[f]
+	return ok
+}
